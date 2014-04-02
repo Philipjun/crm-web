265ae926d18969862f8204371c6a6e5b5799b3cc
Philip Jun <inbox@philipjun.com>
2014-04-01 22:50:03 -0400

    added contacts

--- crm.rb
+++ crm.rb
@@ -2,4 +2,13 @@ require_relative 'contact'
 require 'sinatra'
 
 get '/' do
-...
+  end
+
+get "/contacts" do
+  @contacts = []
+  @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
+  @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
+  @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
+
+  erb :contacts
+end
