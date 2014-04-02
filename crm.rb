5113ff229fe92f96bf6ae91d98d450fd3bedde74
Philip Jun <inbox@philipjun.com>
2014-04-02 00:29:46 -0400

    updated

--- crm.rb
+++ crm.rb
@@ -7,15 +7,16 @@ require 'sinatra'
 get '/' do
   end
 
-get "/contacts" do
-  @contacts = []
-  @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
-  @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
-  @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
-
-  erb :contacts
+post '/contacts' do
+  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  @@rolodex.add_contact(new_contact)
+  redirect to('/contacts')
 end
 
 get '/contacts/new' do
   erb :new_contact
+end
+
+post '/contacts' do
+  puts params
 end
