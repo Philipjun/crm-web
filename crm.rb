e1a86ebba2d2da61ba8b5c390ff31dd533af3d5b
Philip Jun <inbox@philipjun.com>
2014-04-03 15:24:14 -0400

    added params

--- crm.rb
+++ crm.rb
@@ -8,10 +8,14 @@ get '/' do
   erb :index
 end
 
-get "/contacts" do
+get '/contacts' do
   erb :contacts
 end
 
 get '/contacts/new' do 
   erb :new_contact
+end
+
+post '/contacts' do
+  puts params
 end
