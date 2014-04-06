82b21f22ecb51cffb819031faaccab33aa45e029
Philip Jun <inbox@philipjun.com>
2014-04-06 14:44:13 -0400

    updated

--- crm.rb
+++ crm.rb
@@ -1,9 +1,20 @@
+require_relative 'rolodex'
 
 require 'sinatra'
 require 'data_mapper'
 
-DataMapper.setup(:detfault, "sqlite3:database.sqlite3")
+DataMapper.setup(:default, "sqlite3:database.sqlite3")
 
+class Contact
+  attr_accessor :id, :first_name, :last_name, :email, :note
+
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+  end
+end
 
 get '/' do
   erb :index
