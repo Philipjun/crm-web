9ed6a1d9a745e476d9156d9e4c6e06a36c4b313a
Philip Jun <inbox@philipjun.com>
2014-04-06 16:35:37 -0400

    deleted files

--- crm.rb
+++ crm.rb
@@ -1,4 +1,4 @@
-require_relative 'rolodex'
+
 
 require 'sinatra'
 require 'data_mapper'
@@ -32,7 +32,7 @@ get '/contacts/new' do
 end
 
 post "/contacts" do
-  contact = Contact.create(
+  @contact = Contact.create(
     :first_name => params[:first_name],
     :last_name => params[:last_name],
     :email => params[:email],
@@ -42,7 +42,7 @@ post "/contacts" do
 end
 
 get "/contacts/:id" do
-  @contact = @Contact.get(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
     erb :show_contact
   else
@@ -54,7 +54,7 @@ end
 
 
 get "/contacts/:id/edit" do 
-  @contact =@@rolodex.find(params[:id].to_i)
+  @contact =Contact.get(params[:id].to_i)
   if @contact
   erb :edit_contact
 else
@@ -64,12 +64,12 @@ end
 
 
 put "/contacts/:id" do
-  @contact = @Contact.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
-    @contact.first_name = params[:first_name]
-    @contact.last_name = params[:last_name]
-    @contact.email = params[:email]
-    @contact.note = params[:note]
+    @contact.update(:first_name => params[:first_name])
+    @contact.update(:last_name => params[:last_name])
+    @contact.update(:email => params[:email])
+    @contact.update(:note => params[:note])
 
     redirect to("/contacts")
   else
@@ -78,9 +78,9 @@ put "/contacts/:id" do
 end
 
 delete "/contacts/:id" do
-  @contact = @contact.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
-    @@rolodex.remove_contact(@contact)
+    @contact.destroy
     redirect to("/contacts")
   else
     raise Sinatra::NotFound
