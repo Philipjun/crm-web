746d576d6addf199f4a2ad70ff2e1a86f29dd0ea
Philip Jun <inbox@philipjun.com>
2014-04-06 15:01:37 -0400

    updated

--- crm.rb
+++ crm.rb
@@ -31,14 +31,14 @@ get '/contacts/new' do
   erb :new_contact
 end
 
-post "/contacts" do 
-  contact = Contact.create (
-    :first_name =>params[:first_name],
-    :last_name =>params[:last_name],
-    :email =>params[:email],
-    :note =>params[:note]
-    )
-    redirect to('/contacts')
+post "/contacts" do
+  contact = Contact.create(
+    :first_name => params[:first_name],
+    :last_name => params[:last_name],
+    :email => params[:email],
+    :note => params[:note]
+  )
+  redirect to('/contacts')
 end
 
 get "/contacts/:id" do
