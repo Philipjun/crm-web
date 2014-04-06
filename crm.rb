920ad4724c35dc2fb3bed60acb9415c9d703ccb9
Philip Jun <inbox@philipjun.com>
2014-04-06 15:00:33 -0400

    fetching contact

--- crm.rb
+++ crm.rb
@@ -32,16 +32,16 @@ get '/contacts/new' do
 end
 
 post "/contacts" do 
-  contact = Contact.create(
-    :first_name => params [:first_name],
-    :last_name => params [:last_name],
-    :email => params [:email],
-    :note => params [:note]
+  contact = Contact.create (
+    :first_name =>params[:first_name],
+    :last_name =>params[:last_name],
+    :email =>params[:email],
+    :note =>params[:note]
     )
     redirect to('/contacts')
 end
 
-get '/contacts/:id' do
+get "/contacts/:id" do
   @contact = @Contact.get(params[:id].to_i)
   if @contact
     erb :show_contact
